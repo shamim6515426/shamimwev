fcff6be983c7dbd59b8263d550eadca76693c8fc
shamim6515426 <113198996+shamim6515426@users.noreply.github.com>
2022-11-14 08:25:48 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,15 @@
+function  sound(){
+     var audio= new Audio("1.mp3 ");
+     audio.play();
+    
+    
+    };
+ 
+    
+
+  var body= document.querySelector(" body");
+  
+  function  bacolor(){
+   body.classList.toggle("pata");
+
+  }
